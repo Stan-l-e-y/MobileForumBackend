38336e81c08b33c88571780987d39759c62e43a4
Stanley Tsonev <stanleytsonev@hotmail.com>
2022-12-06 08:46:54 -0500

    login + register routes

--- src/router/routes/user.ts
+++ src/router/routes/user.ts
@@ -1,31 +1,54 @@
 import { Router } from 'express';
 import { prisma } from '../../server.js';
+import bcyprt from 'bcrypt';
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => {
+userRouter.get('/:username', (req, res) => {
   res.send({ message: 'Hello user!' });
 });
 
 userRouter.post('/login', async (req, res) => {
-  const user = await prisma.user.create({
-    data: {
-      email: 'stanley@tsonev.com',
-      username: 'Stanley',
-      password: '123456',
-      posts: {
-        create: {
-          title: 'Hello world!',
-          content: 'This is my first post!',
-        },
-      },
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).send({ message: 'Missing email or password' });
+  }
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
     },
   });
-  res.send(user);
+  if (!user) {
+    res.status(400).send({ message: 'User not found' });
+  } else {
+    const passwordMatch = await bcyprt.compare(password, user.password);
+    if (passwordMatch) {
+      res.send({ message: 'Logged in' });
+    } else {
+      res.status(400).send({ message: 'Wrong password' });
+    }
+  }
 });
 
-userRouter.post('/register', (req, res) => {
-  res.send({ message: 'Hello user!' });
+userRouter.post('/register', async (req, res) => {
+  const { email, password, username } = req.body;
+  if (!email || !password || !username) {
+    res.status(400).send({ message: 'Missing email, password or username' });
+  }
+
+  const hashedPassword = bcyprt.hashSync(password, 10);
+  try {
+    await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        username,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({ message: 'User already exists' });
+  }
+  res.send({ message: 'User created' });
 });
 
 export { userRouter };
