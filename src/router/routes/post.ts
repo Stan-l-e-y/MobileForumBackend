c43f9ec63eeeec08e623629246f120258181536b
Stanley Tsonev <stanleytsonev@hotmail.com>
2022-12-05 19:07:39 -0500

    basic routes

--- src/router/routes/post.ts
+++ src/router/routes/post.ts
@@ -2,4 +2,24 @@ import { Router } from 'express';
 
 const postRouter = Router();
 
+postRouter.get('/posts', (req, res) => {
+  res.send({ message: 'Hello get all post!' });
+});
+
+postRouter
+  .route('/:id')
+  .get((req, res) => {
+    res.send({ message: 'Hello get single post!' });
+  })
+  .put((req, res) => {
+    res.send({ message: 'Hello put post!' });
+  })
+  .delete((req, res) => {
+    res.send({ message: 'Hello delete post!' });
+  });
+
+postRouter.post('/create', (req, res) => {
+  res.send({ message: 'Hello post post!' });
+});
+
 export default postRouter;
--- src/router/routes/user.ts
+++ src/router/routes/user.ts
@@ -6,4 +6,12 @@ userRouter.get('/', (req, res) => {
   res.send({ message: 'Hello user!' });
 });
 
+userRouter.post('/login', (req, res) => {
+  res.send({ message: 'Hello user!' });
+});
+
+userRouter.post('/register', (req, res) => {
+  res.send({ message: 'Hello user!' });
+});
+
 export { userRouter };
